df8dc6c8a33f776fb5d542f3dd7aef5a4cf64acb
infrapsp-sdk <sdk@infrapsp.com.br>
2024-08-14 17:46:14 +0000

    Update SDK

--- modules/types/payable/find_payable_request.ts
+++ modules/types/payable/find_payable_request.ts
@@ -6,35 +6,40 @@ import { SortOrder } from '../../../modules/types/base/types.ts';
 
 export const FindPayableParamsSchema = BaseParamsSchema;
 
-export const FindPayableQuerySchema = BaseQuerySchema.and(
-  z.object({
-    transactionId: ZodSchemas.nanoid().optional(),
-    transactionRefundId: ZodSchemas.nanoid().optional(),
-    transactionSplitId: ZodSchemas.nanoid().optional(),
-    transferId: ZodSchemas.nanoid().optional(),
-    type: z.nativeEnum(PayableType).optional(),
-    method: z.nativeEnum(PayableMethod).optional(),
-    status: ZodSchemas.stringArray(z.nativeEnum(PayableStatus)).optional(),
-    notStatus: ZodSchemas.stringArray(z.nativeEnum(PayableStatus)).optional(),
-    sortField: z.enum(['createdAt', 'paymentDate']).default('createdAt'),
-    sortOrder: z.nativeEnum(SortOrder).default(SortOrder.DESC),
-    paymentDateLte: ZodSchemas.datetime().optional(),
-    paymentDateGte: ZodSchemas.datetime().optional(),
+export const FindPayableQuerySchema = z.object({
+  entity: z.enum(['transfer', 'transaction', 'transactionSplit', 'transactionRefund']),
+  entityId: ZodSchemas.nanoid(),
+}).or(
+  BaseQuerySchema.and(
+    z.object({
+      transactionId: ZodSchemas.nanoid().optional(),
+      transactionRefundId: ZodSchemas.nanoid().optional(),
+      transactionSplitId: ZodSchemas.nanoid().optional(),
+      transferId: ZodSchemas.nanoid().optional(),
+      type: z.nativeEnum(PayableType).optional(),
+      method: z.nativeEnum(PayableMethod).optional(),
+      status: ZodSchemas.stringArray(z.nativeEnum(PayableStatus)).optional(),
+      notStatus: ZodSchemas.stringArray(z.nativeEnum(PayableStatus)).optional(),
+      sortField: z.enum(['createdAt', 'paymentDate']).default('createdAt'),
+      sortOrder: z.nativeEnum(SortOrder).default(SortOrder.DESC),
+      paymentDateLte: ZodSchemas.datetime().optional(),
+      paymentDateGte: ZodSchemas.datetime().optional(),
+    }),
+  ).transform((dto, ctx) => {
+    ZodRefines.validDateRange(ctx, dto.createdAtGte, dto.createdAtLte, 1000 * 60 * 60 * 24 * 30 * 12, 'createdAt');
+
+    // paymentDate validation
+    if ((dto.paymentDateGte && !dto.paymentDateLte) || (dto.paymentDateLte && !dto.paymentDateGte)) {
+      ZodHelpers.issue(ctx, 'paymentDate', 'should inform both paymentDateGte and paymentDateLte');
+    }
+
+    if (dto.paymentDateGte && dto.paymentDateLte) {
+      ZodRefines.validDateRange(ctx, dto.paymentDateGte, dto.paymentDateLte, 1000 * 60 * 60 * 24 * 30 * 12, 'paymentDate');
+    }
+
+    return dto;
   }),
-).transform((dto, ctx) => {
-  ZodRefines.validDateRange(ctx, dto.createdAtGte, dto.createdAtLte, 1000 * 60 * 60 * 24 * 30 * 12, 'createdAt');
-
-  // paymentDate validation
-  if ((dto.paymentDateGte && !dto.paymentDateLte) || (dto.paymentDateLte && !dto.paymentDateGte)) {
-    ZodHelpers.issue(ctx, 'paymentDate', 'should inform both paymentDateGte and paymentDateLte');
-  }
-
-  if (dto.paymentDateGte && dto.paymentDateLte) {
-    ZodRefines.validDateRange(ctx, dto.paymentDateGte, dto.paymentDateLte, 1000 * 60 * 60 * 24 * 30 * 12, 'paymentDate');
-  }
-
-  return dto;
-});
+);
 
 export type FindPayableParamsDto = z.infer<typeof FindPayableParamsSchema>;
 export type FindPayableQueryDto = z.infer<typeof FindPayableQuerySchema>;
--- modules/types/payable/payable_aggregation_response.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'https://deno.land/x/zod@v3.23.4/mod.ts';
-
-export const PayableAggregationResponseSchema = z.object({
-  merchantAmount: z.number().int(),
-  feeAmount: z.number().int(),
-});
-
-export type PayableAggregationResponseDto = z.infer<typeof PayableAggregationResponseSchema>;

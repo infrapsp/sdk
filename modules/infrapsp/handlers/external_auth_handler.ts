54e147665a16d0a0c9cda27efe4e809387f784d7
infrapsp-sdk <sdk@infrapsp.com.br>
2024-08-21 18:36:24 +0000

    Update SDK

--- modules/infrapsp/handlers/external_auth_handler.ts
+++ modules/infrapsp/handlers/external_auth_handler.ts
@@ -1,19 +1,19 @@
 import { KyInstance, Options } from 'npm:ky@1.2.4';
 import { AsyncResult } from '../../../modules/types/result.ts';
 import { validateResponse } from '../../../modules/infrapsp/validate_response.ts';
-import { ApiKeyResponseDto } from '../../../modules/types/api_key/api_key_response.ts';
+import { ExternalAuthMerchantResponseDto } from '../../../modules/types/external_auth/external_auth_merchant_response.ts';
 
 export class ExternalAuthHandler {
   private readonly basePath = 'v1/auth/external-auth/merchants';
 
   constructor(private readonly kyInstance: KyInstance) {}
 
-  async findMany(options?: Options): AsyncResult<ApiKeyResponseDto[]> {
+  async findMany(options?: Options): AsyncResult<ExternalAuthMerchantResponseDto[]> {
     const url = this.basePath;
 
     const response = await this.kyInstance.get(url, options);
 
-    const data = await response.json<ApiKeyResponseDto[]>();
+    const data = await response.json<ExternalAuthMerchantResponseDto[]>();
 
     return validateResponse({ data, status: response.status });
   }

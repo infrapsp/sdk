e356fe48c28d0bd38780a10768a522d87d0276ff
infrapsp-sdk <sdk@infrapsp.com.br>
2026-01-26 16:00:42 +0000

    Update SDK

---
+++ modules/utils/deep_parse_json.ts
@@ -0,0 +1,45 @@
+// deno-lint-ignore-file no-explicit-any
+const isNumString = (str: string) => !isNaN(Number(str));
+
+// Checks if string is in YYYY-MM-DDTHH:MM:SS.sssZ format
+const isDateString = (str: string) => {
+  const _regExp = new RegExp(
+    /^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(?:\.\d+)?(?:Z|[+-](?:2[0-3]|[01][0-9]):?[0-5][0-9])?$/,
+  );
+  return _regExp.test(str);
+};
+
+export function deepParseJson<T = any>(jsonString: number | string | Record<string, any>): T {
+  // if not stringified json rather a simple string value then JSON.parse will throw error
+  // otherwise continue recursion
+  if (typeof jsonString === 'string') {
+    if (jsonString === 'null') {
+      return jsonString as any;
+    }
+
+    if (isNumString(jsonString)) {
+      // if a numeric string is received, return itself
+      // otherwise JSON.parse will convert it to a number
+      return jsonString as any;
+    }
+    try {
+      return deepParseJson<T>(JSON.parse(jsonString));
+    } catch {
+      return jsonString as any;
+    }
+  } else if (Array.isArray(jsonString)) {
+    // if an array is received, map over the array and deepParse each value
+    return jsonString.map((val) => deepParseJson<T>(val)) as any;
+  } else if (typeof jsonString === 'object' && jsonString !== null) {
+    // if an object is received then deepParse each element in the object
+    // typeof null returns 'object' too, so we have to eliminate that
+    return Object.keys(jsonString).reduce((obj: any, key: string) => {
+      const val = jsonString[key];
+      obj[key] = isNumString(val) ? val : isDateString(val) ? new Date(val) : deepParseJson(val);
+      return obj;
+    }, {} as T);
+  } else {
+    // otherwise return whatever was received
+    return jsonString as any;
+  }
+}

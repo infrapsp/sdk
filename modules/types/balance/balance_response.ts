6ae29efb32191ecb7618b51ed64a60c856588cf3
infrapsp-sdk <sdk@infrapsp.com.br>
2026-03-05 13:25:23 +0000

    Update SDK

--- modules/types/balance/balance_response.ts
+++ modules/types/balance/balance_response.ts
@@ -1,7 +1,7 @@
 import { z } from 'npm:@hono/zod-openapi@1.1.0';
 
 export const BalanceResponseSchema = z.record(
-  z.enum(['pix', 'creditCard']),
+  z.enum(['tenant', 'slc']),
   z.object({
     amountAvailable: z.number(),
     amountWaitingFunds: z.number(),

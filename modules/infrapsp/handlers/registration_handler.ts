bd527fd1df786178da0251c2e54c98ce724685f6
infrapsp-sdk <sdk@infrapsp.com.br>
2026-08-19 13:13:41 +0000

    Update SDK

--- modules/infrapsp/handlers/registration_handler.ts
+++ modules/infrapsp/handlers/registration_handler.ts
@@ -2,6 +2,7 @@ import { AsyncResult } from '../../../modules/types/result.ts';
 import { validateResponse } from '../../../modules/infrapsp/validate_response.ts';
 import { RegistrationResponseDto } from '../../../modules/types/registration/registration_response.ts';
 import { FindRegistrationQuerySchema } from '../../../modules/types/registration/find_registration_request.ts';
+import { UpdateRegistrationBodySchema } from '../../../modules/types/registration/update_registration_request.ts';
 import type { HttpClient } from '../../../modules/http/http_client.ts';
 import type { z } from 'npm:@hono/zod-openapi@1.4.0';
 import { isError } from '../../../modules/errors/is_error.ts';
@@ -20,6 +21,29 @@ export class RegistrationHandler {
     const url = query ? this.basePath + '?' + queryPath : this.basePath;
 
     const response = await this.httpClient.get(url, requestInit);
+
+    if (isError(response)) return response;
+
+    const data = await response.json();
+
+    return validateResponse({ data, status: response.status });
+  }
+
+  async update(
+    id: string,
+    body: z.input<typeof UpdateRegistrationBodySchema>,
+    requestInit: RequestInit = {},
+  ): AsyncResult<RegistrationResponseDto> {
+    const url = `${this.basePath}/${id}`;
+
+    const response = await this.httpClient.patch(url, {
+      ...requestInit,
+      body: JSON.stringify(body),
+      headers: {
+        ...requestInit.headers,
+        'Content-Type': 'application/json',
+      },
+    });
 
     if (isError(response)) return response;
 
